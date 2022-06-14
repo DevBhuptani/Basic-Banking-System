709565aa001000be17772e5902930b590d88357e
DevBhuptani <190770116008@socet.edu.in>
2022-06-14 15:04:35 +0530

    master

--- pages/connection.php
+++ pages/connection.php
@@ -1,9 +1,9 @@
 <?php
- $servername = "localhost";
+ $host = "localhost";
  $username = "root";
- $password = "root";
+ $password = "password";
  $dbname = "bank";    
- $conn = new mysqli($servername, $username, $password, $dbname);
+ $conn = new mysqli($host, $username, $password, $dbname);
  if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
  }
